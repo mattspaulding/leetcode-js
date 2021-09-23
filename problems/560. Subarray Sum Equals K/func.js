984045f09b009cfd00749138f2fd6998a5e49eef
Matt Spaulding <mrmattspaulding@gmail.com>
2021-09-22 21:09:04 -0400

    adds Subarray Sum Equals K

---
+++ problems/560. Subarray Sum Equals K/func.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var subarraySum = function (nums, k) {
+  let result = 0;
+  let sum = 0;
+  let map = new Map();
+  map.set(0, 1);
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+    if (map.has(sum - k)) {
+      result += map.get(sum - k);
+    }
+    map.set(sum, (map.get(sum) || 0) + 1);
+  }
+  return result;
+};
+
+module.exports = {
+  func: subarraySum,
+};
