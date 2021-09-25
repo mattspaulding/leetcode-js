ba2af28d27662c218ca51091977389071aa1e4a3
Matt Spaulding <mrmattspaulding@gmail.com>
2021-09-25 17:28:37 -0400

    adds Binary Tree Inorder Traversal

---
+++ problems/94. Binary Tree Inorder Traversal/func.test.js
@@ -0,0 +1,22 @@
+const { func } = require("./func");
+
+test("Example 1", () => {
+  // [1,null,2,3]
+  const root = {
+    val: 1,
+    left: null,
+    right: {
+      val: 2,
+      left: {
+        val: 3,
+        left: null,
+        right: null,
+      },
+      right: null,
+    },
+  };
+
+  const expected = [1, 3, 2];
+
+  expect(func(root)).toEqual(expected);
+});
